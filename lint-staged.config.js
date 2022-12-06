fdb02fe214d29d0ab473452f02a90d704113ea6f
lotyp <lotyp7@gmail.com>
2022-12-06 14:02:57 +0200

    fix: cant find types for jest-dom

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 // More info — https://rizkicitra.dev/blog/how-to-setup-husky-nextjs
 module.exports = {
     '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint', 'prettier --config ./.prettierrc.js --write'],
-    // '**/*.ts?(x)': () => 'yarn check-types',
+    '**/*.ts?(x)': () => 'yarn check-types',
     '*.json': ['prettier --config ./.prettierrc.js --write'],
     '**/*.{css,scss,md,html,json}': ['prettier --config ./.prettierrc.js --write'],
 }
